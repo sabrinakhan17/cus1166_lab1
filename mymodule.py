9d9d673ae948c430b695b0fe8ca0efaa76cf2ee9
skhan667 <sabrina.khan17@stjohns.edu>
2019-09-15 22:29:46 -0400

    defining modules

---
+++ mymodule.py
@@ -0,0 +1,11 @@
+# file: mymodule.helper_utils
+# A function define in the module.
+
+def square(x):
+    return x*x
+# method to execute file is run from command line.
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
